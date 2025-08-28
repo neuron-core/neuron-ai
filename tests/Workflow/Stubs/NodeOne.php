9b91ecea20e0a591ec40a98e092a9b5312ccd35e
Valerio Barbera <valerio@inspector.dev>
2025-08-28 18:59:44 +0200

    workflow tests

--- tests/Workflow/Stubs/NodeOne.php
+++ tests/Workflow/Stubs/NodeOne.php
@@ -6,10 +6,14 @@ namespace NeuronAI\Tests\Workflow\Stubs;
 
 use NeuronAI\Workflow\Event;
 use NeuronAI\Workflow\Node;
+use NeuronAI\Workflow\StartEvent;
 use NeuronAI\Workflow\WorkflowState;
 
 class NodeOne extends Node
 {
+    /**
+     * @param StartEvent $event
+     */
     public function run(Event $event, WorkflowState $state): FirstEvent
     {
         $state->set('node_one_executed', true);
