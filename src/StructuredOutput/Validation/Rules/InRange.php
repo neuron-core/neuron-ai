da1392a37f8c9c05090e39c0d26c898193c055de
Valerio Barbera <valerio@inspector.dev>
2025-08-31 09:56:09 +0200

    validation rules

--- src/StructuredOutput/Validation/Rules/InRange.php
+++ src/StructuredOutput/Validation/Rules/InRange.php
@@ -7,7 +7,7 @@ namespace NeuronAI\StructuredOutput\Validation\Rules;
 #[\Attribute(\Attribute::TARGET_PROPERTY)]
 class InRange extends AbstractValidationRule
 {
-    public function __construct(protected int|float $min, protected int|float $max, protected bool $allowEquals = true)
+    public function __construct(protected int|float $min, protected int|float $max, protected bool $strict = false)
     {
     }
 
@@ -15,13 +15,13 @@ class InRange extends AbstractValidationRule
     {
         if ($value < $this->min) {
             $violations[] = $this->buildMessage($name, 'must be greater or equals to {compare}', ['compare' => \get_debug_type($this->min)]);
-        } elseif (!$this->allowEquals && $value === $this->min) {
+        } elseif ($this->strict && $value === $this->min) {
             $violations[] = $this->buildMessage($name, 'must be strictly greater than {compare}', ['compare' => \get_debug_type($this->min)]);
         }
 
         if ($value > $this->max) {
             $violations[] = $this->buildMessage($name, 'must be lower than {compare}', ['compare' => \get_debug_type($this->max)]);
-        } elseif (!$this->allowEquals && $value === $this->max) {
+        } elseif ($this->strict && $value === $this->max) {
             $violations[] = $this->buildMessage($name, 'must be strictly lower than {compare}', ['compare' => \get_debug_type($this->max)]);
         }
     }
