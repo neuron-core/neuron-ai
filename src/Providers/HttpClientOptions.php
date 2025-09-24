59f342223330ca0206de38f13a23bd53b425bc37
Valerio Barbera <valerio@inspector.dev>
2025-09-24 12:31:17 +0200

    http options float numbers

--- src/Providers/HttpClientOptions.php
+++ src/Providers/HttpClientOptions.php
@@ -10,8 +10,8 @@ class HttpClientOptions
      * @param array<string, string|int|float>|null $headers
      */
     public function __construct(
-        public readonly ?int $timeout = null,
-        public readonly ?int $connectTimeout = null,
+        public readonly ?float $timeout = null,
+        public readonly ?float $connectTimeout = null,
         public readonly ?array $headers = null,
     ) {
     }
