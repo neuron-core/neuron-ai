7db9e75e0dd284bca73c006d32f0ae8509efe506
Valerio Barbera <valerio@inspector.dev>
2025-10-17 17:16:23 +0200

    chromadb tests

--- src/RAG/VectorStore/ChromaVectorStore.php
+++ src/RAG/VectorStore/ChromaVectorStore.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace NeuronAI\RAG\VectorStore;
 
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\RequestOptions;
 use NeuronAI\RAG\Document;
 use NeuronAI\RAG\VectorSimilarity;
@@ -13,14 +14,35 @@ class ChromaVectorStore implements VectorStoreInterface
 {
     protected Client $client;
 
+    /**
+     * @throws GuzzleException
+     */
     public function __construct(
         protected string $collection,
         protected string $host = 'http://localhost:8000',
         protected int $topK = 5,
     ) {
+        $this->initialize();
     }
 
-    protected function getClient(): Client
+    /**
+     * Create the collection if it doesn't exist
+     *
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    protected function initialize(): void
+    {
+        try {
+            $this->client()->get('');
+        } catch (\Exception $e) {
+            $client = new Client(['base_uri' => \trim($this->host, '/').'/api/v1/']);
+            $client->post('collections', [
+                RequestOptions::JSON => ['name' => $this->collection]
+            ]);
+        }
+    }
+
+    protected function client(): Client
     {
         return $this->client ?? $this->client = new Client([
             'base_uri' => \trim($this->host, '/')."/api/v1/collections/{$this->collection}/",
@@ -37,7 +59,7 @@ class ChromaVectorStore implements VectorStoreInterface
 
     public function deleteBySource(string $sourceType, string $sourceName): VectorStoreInterface
     {
-        $this->getClient()->post('delete', [
+        $this->client()->post('delete', [
             RequestOptions::JSON => [
                 'where' => [
                     'sourceType' => $sourceType,
@@ -51,7 +73,7 @@ class ChromaVectorStore implements VectorStoreInterface
 
     public function addDocuments(array $documents): VectorStoreInterface
     {
-        $this->getClient()->post('add', [
+        $this->client()->post('add', [
             RequestOptions::JSON => $this->mapDocuments($documents),
         ]);
 
@@ -60,7 +82,7 @@ class ChromaVectorStore implements VectorStoreInterface
 
     public function similaritySearch(array $embedding): iterable
     {
-        $response = $this->getClient()->post('query', [
+        $response = $this->client()->post('query', [
             RequestOptions::JSON => [
                 'query_embeddings' => $embedding,
                 'n_results' => $this->topK,
