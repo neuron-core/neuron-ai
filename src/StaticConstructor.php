7557810c51d259b3cf64fbbac37becd4b3f54712
Kyle Arch <kaarch@gmail.com>
2025-10-26 06:17:47 -0700

    Fix StaticConstructor::make() to support named parameters
    - Add named parameter detection and conversion to positional arguments
    - Use reflection to map parameter names to constructor positions
    - Handle default values for optional parameters
    - Fixes MCP functionality that uses named parameters with Tool::make()
    
    Resolves #341

--- src/StaticConstructor.php
+++ src/StaticConstructor.php
@@ -11,6 +11,39 @@ trait StaticConstructor
      */
     public static function make(...$arguments): static
     {
+        // Handle named parameters by converting them to positional arguments
+        if (count($arguments) === 1 && is_array($arguments[0]) && array_keys($arguments[0]) !== range(0, count($arguments[0]) - 1)) {
+            // This is an associative array (named parameters)
+            $namedArgs = $arguments[0];
+
+            // Use reflection to map named parameters to constructor positions
+            $reflection = new \ReflectionClass(static::class);
+            $constructor = $reflection->getConstructor();
+
+            if ($constructor) {
+                $params = $constructor->getParameters();
+                $positionalArgs = [];
+
+                foreach ($params as $param) {
+                    $paramName = $param->getName();
+                    if (array_key_exists($paramName, $namedArgs)) {
+                        $positionalArgs[] = $namedArgs[$paramName];
+                    } else {
+                        // Use default value if available
+                        if ($param->isDefaultValueAvailable()) {
+                            $positionalArgs[] = $param->getDefaultValue();
+                        } else {
+                            // Parameter is required but not provided
+                            throw new \ArgumentCountError("Missing required parameter: {$paramName}");
+                        }
+                    }
+                }
+
+                /** @phpstan-ignore new.static */
+                return new static(...$positionalArgs);
+            }
+        }
+
         /** @phpstan-ignore new.static */
         return new static(...$arguments);
     }
