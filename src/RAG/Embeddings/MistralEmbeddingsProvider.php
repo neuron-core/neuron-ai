7bc7842dffd4ef05123fe12b5ecc45ab66685374
Valerio Barbera <valerio@inspector.dev>
2025-11-04 18:23:11 +0100

    mistral embedding provider

---
+++ src/RAG/Embeddings/MistralEmbeddingsProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NeuronAI\RAG\Embeddings;
+
+use GuzzleHttp\Client;
+
+class MistralEmbeddingsProvider extends OpenAIEmbeddingsProvider
+{
+    protected string $baseUri = 'https://api.mistral.ai/v1/embeddings';
+
+    public function __construct(
+        protected string $key,
+        protected string $model = 'mistral-embed',
+        protected ?int $dimensions = 1024
+    ) {
+        parent::__construct($key, $model, $dimensions);
+    }
+}
