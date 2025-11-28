aa26907d9550d51052857779ef3fa701a6eb9024
Valerio Barbera <valerio@inspector.dev>
2025-11-28 14:42:05 +0100

    fix mistral chat with advanced api response

--- src/Providers/Mistral/HandleChat.php
+++ src/Providers/Mistral/HandleChat.php
@@ -7,7 +7,12 @@ namespace NeuronAI\Providers\Mistral;
 use GuzzleHttp\Promise\PromiseInterface;
 use GuzzleHttp\RequestOptions;
 use NeuronAI\Chat\Enums\MessageRole;
+use NeuronAI\Chat\Enums\SourceType;
 use NeuronAI\Chat\Messages\AssistantMessage;
+use NeuronAI\Chat\Messages\ContentBlocks\AudioContent;
+use NeuronAI\Chat\Messages\ContentBlocks\FileContent;
+use NeuronAI\Chat\Messages\ContentBlocks\ImageContent;
+use NeuronAI\Chat\Messages\ContentBlocks\ReasoningContent;
 use NeuronAI\Chat\Messages\ContentBlocks\TextContent;
 use NeuronAI\Chat\Messages\Message;
 use NeuronAI\Chat\Messages\ToolCallMessage;
@@ -46,13 +51,34 @@ trait HandleChat
             ->then(function (ResponseInterface $response): AssistantMessage|ToolCallMessage {
                 $result = json_decode($response->getBody()->getContents(), true);
 
-                if ($result['choices'][0]['finish_reason'] === 'tool_calls') {
+                $choice = $result['choices'][0];
+
+                if ($choice['finish_reason'] === 'tool_calls') {
                     $response = $this->createToolCallMessage(
-                        $result['choices'][0]['message']['tool_calls'],
-                        new TextContent($result['choices'][0]['message']['content'])
+                        $choice['message']['tool_calls'],
+                        new TextContent($choice['message']['content'])
                     );
+                } elseif (\is_string($choice['message']['content'])) {
+                    $response = new AssistantMessage($choice['message']['content']);
                 } else {
-                    $response = new AssistantMessage($result['choices'][0]['message']['content']);
+                    $blocks = [];
+                    foreach ($choice['content'] as $content) {
+                        $blocks[] = match ($content['type']) {
+                            'text' => new TextContent($content['text'] ?? ''),
+                            'thinking' => new ReasoningContent(\array_reduce(\array_filter($content['thinking'], fn(array $item): bool => $item['type'] === 'text'), fn (string $carry, array $item): string => $carry . $item['text'], '')),
+                            'image_url' => new ImageContent(
+                                $content['image_url']['url'] ?? '',
+                                SourceType::BASE64
+                            ),
+                            'document_url' => new FileContent(
+                                content: $content['document_url'] ?? '',
+                                sourceType: SourceType::BASE64,
+                                filename: $content['document_name'] ?? null
+                            ),
+                            'input_audio' => new AudioContent($content['input_audio'], SourceType::BASE64),
+                        };
+                    }
+                    $response = new AssistantMessage($blocks);
                 }
 
                 if (isset($result['usage'])) {
