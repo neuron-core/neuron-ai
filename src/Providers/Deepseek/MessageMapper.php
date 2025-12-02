211924ecf528cdae00504dce7399977c26f6d029
Valerio Barbera <valerio@inspector.dev>
2025-12-02 22:24:58 +0100

    fix tests

--- src/Providers/Deepseek/MessageMapper.php
+++ src/Providers/Deepseek/MessageMapper.php
@@ -6,7 +6,6 @@ namespace NeuronAI\Providers\Deepseek;
 
 use NeuronAI\Chat\Messages\Message;
 use NeuronAI\Chat\Messages\ToolCallMessage;
-use NeuronAI\Chat\Messages\ToolCallResultMessage;
 use NeuronAI\Providers\OpenAI\MessageMapper as OpenAIMessageMapper;
 
 class MessageMapper extends OpenAIMessageMapper
