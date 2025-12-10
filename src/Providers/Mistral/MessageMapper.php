90601f336c6d38096aa888319f641c27b67e8a72
Valerio Barbera <valerio@inspector.dev>
2025-12-10 18:02:36 +0100

    content block mapping for File ID

--- src/Providers/Mistral/MessageMapper.php
+++ src/Providers/Mistral/MessageMapper.php
@@ -6,6 +6,7 @@ namespace NeuronAI\Providers\Mistral;
 
 use NeuronAI\Chat\Attachments\Attachment;
 use NeuronAI\Chat\Enums\AttachmentContentType;
+use NeuronAI\Exceptions\ProviderException;
 use NeuronAI\Providers\OpenAI\MessageMapper as OpenAIMessageMapper;
 
 use function uniqid;
@@ -15,21 +16,23 @@ class MessageMapper extends OpenAIMessageMapper
     public function mapDocumentAttachment(Attachment $document): array
     {
         return match ($document->contentType) {
-            AttachmentContentType::BASE64 => [
+            /*AttachmentContentType::BASE64 => [
                 'type' => 'file',
                 'file' => [
                     'filename' => $document->filename ?? "attachment-".uniqid().".pdf",
                     'file_data' => "data:{$document->mediaType};base64,{$document->content}",
                 ]
-            ],
+            ],*/
             AttachmentContentType::URL => [
                 'type' => 'document',
                 'document_url' => $document->content,
+                'document_name' => $document->filename ?? "attachment-".uniqid().".pdf",
             ],
             AttachmentContentType::ID => [
                 'type' => 'file',
                 'file_id' => $document->content,
             ],
+            default => throw new ProviderException('Could not map document attachment type '.$document->contentType->value),
         };
     }
 }
