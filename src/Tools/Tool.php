0bbf808fab0ab00a8ddb366bac0a069b5228a400
Valerio Barbera <valerio@inspector.dev>
2026-02-05 15:58:06 +0100

    fix interruption

--- src/Tools/Tool.php
+++ src/Tools/Tool.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace NeuronAI\Tools;
 
-use Closure;
 use NeuronAI\Exceptions\MissingCallbackParameter;
 use NeuronAI\Exceptions\ToolCallableNotSet;
 use NeuronAI\StaticConstructor;
