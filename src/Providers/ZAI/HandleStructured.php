b33d5eda15a444e4a94a1ddc345e9a0e83c1cbc0
Valerio Barbera <valerio@inspector.dev>
2026-03-12 14:27:44 +0100

    zai provider

--- src/Providers/ZAI/HandleStructured.php
+++ src/Providers/ZAI/HandleStructured.php
@@ -2,6 +2,7 @@
 
 namespace NeuronAI\Providers\ZAI;
 
+use NeuronAI\Chat\Messages\ContentBlocks\TextContent;
 use NeuronAI\Chat\Messages\Message;
 use NeuronAI\Exceptions\HttpException;
 use NeuronAI\Exceptions\ProviderException;
@@ -17,12 +18,17 @@ trait HandleStructured
         string $class,
         array $response_format,
     ): Message {
-        $response_format['type'] = 'json_object';
-
         $this->parameters = array_replace_recursive($this->parameters, [
-            'response_format' => $response_format
+            'response_format' => 'json_object'
         ]);
 
-        return $this->chat(...(is_array($messages) ? $messages : [$messages]));
+        $messages = is_array($messages) ? $messages : [$messages];
+
+        $last = end($messages);
+        $last->addContent(new TextContent(
+            "Generate a JSON with the following schema: \n\n".json_encode($response_format, JSON_PRETTY_PRINT)
+        ));
+
+        return $this->chat($messages);
     }
 }
