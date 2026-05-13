1928ea98f1ac0568e76bbc58e85d73c37a207b2c
Valerio Barbera <valerio@inspector.dev>
2026-05-13 12:45:59 +0200

    remove tool constructor

--- src/Tools/ToolDefinition.php
+++ src/Tools/ToolDefinition.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 namespace NeuronAI\Tools;
 
 /**
- * @method static static make(string $name, ?string $description = null, array $properties = [], array $parameters = [], array $annotations = [])
+ * @method static static make(string $name, ?string $description = null, ToolPropertyInterface[] $properties = [], array $parameters = [], array $annotations = [])
  */
 class ToolDefinition extends Tool
 {
     public function __construct(
         protected string $name,
         protected ?string $description = null,
+        /** @var ToolPropertyInterface[] */
         protected array $properties = [],
         protected array $parameters = [],
         protected array $annotations = [],
