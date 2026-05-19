207240445ca7d73e8298ea2857f83bd5a4a4fc6a
Md.Miraj Khan <miraj.net.xp@gmail.com>
2026-05-19 14:43:42 +0600

    Include all other Gemini modes that support structured output.

--- src/Providers/Gemini/HandleStructured.php
+++ src/Providers/Gemini/HandleStructured.php
@@ -20,8 +20,13 @@ trait HandleStructured
      * https://ai.google.dev/gemini-api/docs/structured-output?example=recipe#structured_outputs_with_tools
      */
     protected array $supportedModels = [
+        'gemini-3.1-flash-lite-preview',
+        'gemini-3.1-pro-preview',
         'gemini-3-pro-preview',
         'gemini-3-flash-preview',
+        'gemini-2.5-pro',
+        'gemini-2.5-flash',
+        'gemini-2.5-flash-lite'
     ];
 
     public function structured(
